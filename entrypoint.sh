a042473a586b620c885acbc0d677e06fc172aa0c
Yura <63817082+yurakuzo@users.noreply.github.com>
2023-10-27 16:12:22 +0300

    Test update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,8 @@ python3 manage.py migrate
 # Collecting static files, at least for admin page
 python3 manage.py collectstatic --noinput
 
-echo "Creating superuser ${ADMIN_lOGIN}"
-python manage.py createsuperuser --no-input --username ${ADMIN_USER} --email {$ADMIN_USER}@example.com
+echo "Creating superuser $ADMIN_lOGIN"
+python manage.py createsuperuser --no-input --username $ADMIN_USER --email $ADMIN_USER@example.com
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='$ADMIN_USER'); user.set_password('$ADMIN_PASSWORD'); user.save()" | python manage.py shell
 
-exec gunicorn --bind 0.0.0.0:${APP_PORT} mandry.wsgi --reload
+exec gunicorn -b :$APP_PORT -b :2222 mandry.wsgi --reload
