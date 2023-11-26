cab6d674df741296a0546d7e2ec29a1fd673ef66
ivanhorbenkomain <ivanhorbenkomain@gmail.com>
2023-11-26 16:15:01 +0100

    fixed minor issue

--- trip/urls.py
+++ trip/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from trip.views import MyTripsView
 from trip.views import TripCreationView
 from trip.views import TripDetailView
+from trip.views import AllTripsView
+
 
 urlpatterns = [
     path('mytrips/', MyTripsView.as_view(), name='mytrips'),
     path("create-trip/", TripCreationView.as_view(), name="create_trip"),
-    path('trips/<int:pk>/', TripDetailView.as_view(), name='trip_detail')
+    path('<int:pk>/', TripDetailView.as_view(), name='trip_detail'),
+    path('all-trips/', AllTripsView.as_view(), name='all_trips'),
+
 ]
